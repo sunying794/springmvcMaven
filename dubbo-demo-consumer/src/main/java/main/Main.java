fb40ec09ca0d5302fad47b7f9e24749cc271c947
veryhao <515881688@qq.com>
2020-02-16 14:14:17 +0800

    提交

--- dubbo-demo-consumer/src/main/java/main/Main.java
+++ dubbo-demo-consumer/src/main/java/main/Main.java
@@ -12,11 +12,21 @@ public class Main {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException {
+		System.out.println("111111111");
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		context.start();
 		GreetingService greetingService = (GreetingService) context.getBean("greetingService");
 		String greetMessage = greetingService.hello("Eric");
 		System.out.println("Consumer ==> " + greetMessage);
+		greetingService.setData("bbsy3", "3bbsy");
+		System.out.println("bbsy3值:" + greetingService.getData("bbsy3"));
+		
+		greetingService.setData("bbsy2", "2bbsy");
+		System.out.println("bbsy2值:" + greetingService.getData("bbsy2"));
+		
+		System.out.println("bbsy磊值:" + greetingService.getData("bbsy3"));
+		
+		
 		context.destroy();
 	}
 	
